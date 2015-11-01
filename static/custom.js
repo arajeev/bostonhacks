49596717febe7c6a8376d436c73d146fd63e012c
Rebecca Santos <rlsantos@uwaterloo.ca>
2015-11-01 10:58:33 -0500

    Changing UI

---
+++ static/custom.js
@@ -0,0 +1,81 @@
+function updateUsername() {
+  var usernameString = document.getElementById("username").value;
+  if (userKey === "") {
+    // insert username
+    var firebaseArray = new Array();
+    firebaseArray["username"] = usernameString;
+    var newPostRef = myFirebaseRef.push(firebaseArray);
+    userKey = newPostRef.key();
+  } else {
+    // update username
+    myFirebaseRef.child(userKey).child("username").set(usernameString);
+  }
+  var welcomeText = document.getElementById("welcome");
+  welcomeText.innerHTML = "Welcome " + usernameString + "!";
+  myFirebaseRef.once('value', function(dataSnapshot) {
+    dataSnapshot.forEach(function(data){
+      if(data.key() != userKey) {
+        otherKey = data.key();
+      }
+    })
+    if(otherKey != "") {
+      myFirebaseRef.child(otherKey).on('value', function(snapshot) {
+        var theirProgressBar = document.getElementById('otherprogress');
+        theirProgressBar.style.width = snapshot.val().score + "%";
+      })
+    }
+  })
+}
+
+var myFirebaseRef = new Firebase("https://codebattles.firebaseio.com/");
+var userKey = "";
+var otherKey = "";
+var codeSubmission = "";
+var codeFunction;
+var fibonacci;
+var score;
+var yourProgress = 0;
+var theirProgress = 0;
+function assert(condition, message) {
+  //if (!condition) {
+  // message = message || "Assertion failed";
+  // if (typeof Error !== "undefined") {
+  //    throw new Error(message);
+  // }
+  // throw message; // Fallback
+  //}
+  return condition;
+}
+
+function onSubmit() {
+  var newPostRef = myFirebaseRef.push();
+  userKey = newPostRef.key();
+
+  codeSubmission = ace.edit("editor").getValue();
+  myFirebaseRef.child(userKey).child("code").set(codeSubmission);
+  // this makes the function that was
+  // stated in the question
+  eval(codeSubmission);
+  score = testFib();
+  console.log(score);
+  myFirebaseRef.child(userKey).child("score").set(score);
+  yourProgress = score;
+  var yourProgressBar = document.getElementById("selfprogress");
+  yourProgressBar.style.width = yourProgress+"%";
+  //other player joins after you -> the otherKey is empty
+  if(otherKey === "") {
+    myFirebaseRef.once('value', function(dataSnapshot) {
+      dataSnapshot.forEach(function(data){
+        if(data.key() != userKey) {
+          otherKey = data.key();
+        }
+      });
+      if(otherKey != "") {
+        myFirebaseRef.child(otherKey).on('value', function(snapshot) {
+          var theirProgressBar = document.getElementById('otherprogress');
+          theirProgressBar.style.width = snapshot.val().score + "%";
+        })
+      }
+    })
+  }
+}
